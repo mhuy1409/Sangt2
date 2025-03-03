4f30a1636f1498071eb851643a89afb5d98941ba
hoangdz2030 <huuhoang1414@gmail.com>
2025-03-03 08:57:45 +0700

    Update

--- ktgiuaki/Form1.Designer.cs
+++ ktgiuaki/Form1.Designer.cs
@@ -28,20 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Username = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Location = new System.Drawing.Point(152, 39);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(70, 16);
+            this.Username.TabIndex = 0;
+            this.Username.Text = "Username";
+            this.Username.Click += new System.EventHandler(this.Username_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(248, 36);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 22);
+            this.txtUsername.TabIndex = 1;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.Username);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Username;
+        private System.Windows.Forms.TextBox txtUsername;
     }
 }
 
--- ktgiuaki/Form1.cs
+++ ktgiuaki/Form1.cs
@@ -16,5 +16,10 @@ namespace ktgiuaki
         {
             InitializeComponent();
         }
+
+        private void Username_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
